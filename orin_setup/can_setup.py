a9caf6e4deb02de7105b00a95b3ea7fd664a0c00
kimku-0112 <66875200+kimku-0112@users.noreply.github.com>
2024-06-02 09:35:07 +0900

    Update can_setup.py

--- orin_setup/can_setup.py
+++ orin_setup/can_setup.py
@@ -1,11 +1,10 @@
 import can
 import subprocess
 
-def configure_can_fd(interface='can0', bitrate='500000', dbitrate='500000'):
+def configure_can_fd(interface='can0', bitrate='1000000', dbitrate='2000000'):
     # Load necessary CAN modules (if not already loaded)
     subprocess.run(['sudo', 'modprobe', 'can'])
     subprocess.run(['sudo', 'modprobe', 'can_raw'])
-    subprocess.run(['sudo', 'modprobe', 'can_fd'])
 
     # Bring down the interface
     subprocess.run(['sudo', 'ip', 'link', 'set', interface, 'down'])
